26cf6c83db4e5a5a94bd086a0d76aa42ad5222a5
Kirk Hawley <leekirkhawley@gmail.com>
2020-04-06 22:34:41 -0700

    initial User List working

--- GigTracker/Startup.cs
+++ GigTracker/Startup.cs
@@ -17,7 +17,7 @@ namespace GigTracker {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddTransient<IUserRepository, FakeUserRepository>();
 			services.AddTransient<IGigRepository, FakeGigRepository>();
-			services.AddMvc();
+			services.AddMvc(options => options.EnableEndpointRouting = false);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,39 +31,14 @@ namespace GigTracker {
 
 			}
 
+			app.UseStatusCodePages();
 			app.UseStaticFiles();
-
-			// Runs matching. An endpoint is selected and set on the HttpContext if a match is found.
-			app.UseRouting();
-
-			// Middleware that run after routing occurs. Usually the following appear here:
-			//app.UseAuthentication();
-			//app.UseAuthorization();
-			//app.UseCors();
-			// These middleware can take different actions based on the endpoint.
-
-			// Executes the endpoint that was selected by routing.
-			app.UseEndpoints(endpoints =>
-			{
-				// Mapping of endpoints goes here:
-				endpoints.MapGet("/", async context => {
-				});
-
-				endpoints.MapControllers();
-				endpoints.MapRazorPages();
-				//endpoints.MapHub<MyChatHub>();
-				//endpoints.MapGrpcService<MyCalculatorService>();
-		
+			app.UseMvc(routes => {
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=User}/{action=List}/{id?}");
 			});
 
-			// Middleware here will only run if nothing was matched.
-
-			app.UseStatusCodePages();
-			//app.UseMvc(routes => { 
-
-			//});
-
-			app.UseRouting();
 
 		}
 	}
