3850d797a03b390962582d852225c09f3f9bdc48
Kirk Hawley <leekirkhawley@gmail.com>
2020-04-06 21:21:03 -0700

    Config stuffs per SportsStore in MFC book; User Model

---
+++ GigTracker/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigTracker.Models {
+	public class User {
+		string UserName { get; set; }
+		string Password { get; set; }
+		string FirstName { get; set; }
+		string LastName { get; set; }
+		string Email { get; set; }
+	}
+}
--- GigTracker/Startup.cs
+++ GigTracker/Startup.cs
@@ -13,21 +13,54 @@ namespace GigTracker {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
+				//app.UseDatabaseErrorPage();
 			}
+			else {
+				app.UseExceptionHandler("/Error");
+
+			}
+
+			app.UseStaticFiles();
+
+			// Runs matching. An endpoint is selected and set on the HttpContext if a match is found.
+			app.UseRouting();
+
+			// Middleware that run after routing occurs. Usually the following appear here:
+			//app.UseAuthentication();
+			//app.UseAuthorization();
+			//app.UseCors();
+			// These middleware can take different actions based on the endpoint.
+
+			// Executes the endpoint that was selected by routing.
+			app.UseEndpoints(endpoints =>
+			{
+				// Mapping of endpoints goes here:
+				endpoints.MapGet("/", async context => {
+				});
+
+				endpoints.MapControllers();
+				endpoints.MapRazorPages();
+				//endpoints.MapHub<MyChatHub>();
+				//endpoints.MapGrpcService<MyCalculatorService>();
+		
+			});
+
+			// Middleware here will only run if nothing was matched.
+
+			app.UseStatusCodePages();
+			//app.UseMvc(routes => { 
+
+			//});
 
 			app.UseRouting();
 
-			app.UseEndpoints(endpoints => {
-				endpoints.MapGet("/", async context => {
-					await context.Response.WriteAsync("Hello World!");
-				});
-			});
 		}
 	}
 }
