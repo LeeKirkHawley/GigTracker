45f0691e43a176c2e6cbf42feea57b542853fe0a
Kirk Hawley <leekirkhawley@gmail.com>
2020-05-08 13:54:05 -0700

    styles for edit pages; new gig working and redirecting to list view

--- GigTracker/Controllers/GigController.cs
+++ GigTracker/Controllers/GigController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using GigTracker.Models;
 using GigTracker.Repositories;
@@ -70,7 +69,20 @@ namespace GigTracker.Controllers {
 
 		[HttpGet("Gig/Create")]
 		public ViewResult Create() {
-			GigCreateViewModel model = new GigCreateViewModel();
+
+
+			Gig gig = new Gig {
+				ArtistName = "Fred",
+				VenueName = "Sam's",
+				VenueAddress = "322 E. 1st. South",
+				VenuePhone = "6027519025",
+				Date = DateTime.Now.AddDays(30)
+			};
+
+			GigCreateViewModel model = new GigCreateViewModel {
+				Gig = gig
+			};
+
 			return View(model);
 		}
 
@@ -84,10 +96,10 @@ namespace GigTracker.Controllers {
 
 			var newGig = await _gigRepository.Add(model.Gig);
 
-			return View($"~/Gig/Details/{newGig.Id}");
+			return RedirectToAction("Details", "Gig", new { id = newGig.Id });
 		}
 
-		[HttpGet("~/Gig/Details/{id}")]
+		[HttpGet("Gig/Details/{id}")]
 		public ActionResult Details(int id) {
 
 			Gig gig = _gigRepository.Get(id).Result;
@@ -101,5 +113,51 @@ namespace GigTracker.Controllers {
 			var v = View(model);
 			return v;
 		}
+
+		[HttpGet("Gig/Edit/{id}")]
+		public ActionResult Edit(int id) {
+
+			string userId = this.HttpContext.Session.GetString("UserId");
+
+			Gig gig = _gigRepository.Get(id).Result;
+
+			if (gig.UserId != Convert.ToInt32(userId)) {
+				return Content("ERROR - user cannot edit this Gig.");
+			}
+
+			gig.User = _userService.GetById(Convert.ToInt32(gig.UserId));
+
+			GigEditViewModel model = new GigEditViewModel {
+				Gig = gig
+			};
+
+			var v = View(model);
+			return v;
+		}
+
+		[HttpPost("Gig/Edit")]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(GigEditViewModel model) {
+
+			string userId = this.HttpContext.Session.GetString("UserId");
+
+			//Gig gig = _gigRepository.Get(id).Result;
+
+			//if (gig.UserId != Convert.ToInt32(userId)) {
+			//	return Content("ERROR - user cannot edit this Gig.");
+			//}
+
+			//gig.User = _userService.GetById(Convert.ToInt32(gig.UserId));
+
+			//GigEditViewModel model = new GigEditViewModel {
+			//	Gig = gig
+			//};
+
+			//var v = View(model);
+			//return v;
+
+			return RedirectToAction("Gig/List");
+		}
+
 	}
 }
---
+++ GigTracker/Models/GigEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GigTracker.Entities;
+
+namespace GigTracker.Models {
+	public class GigEditViewModel {
+		public Gig Gig { get; set; }
+		public string ErrorMsg { get; set; }
+
+	}
+}
